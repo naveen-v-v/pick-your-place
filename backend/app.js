f19184c4e5eac8eeb4c7b928b97e827f87ecaa9f
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-04-06 17:23:26 -0400

    places service

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/places", async (req, res) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const fileContent = await fs.readFile("./data/places.json");
 
--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,11 +1,9 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { map } from 'rxjs';
-
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -17,43 +15,38 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
-  error = signal('')
-  private httpClient = inject(HttpClient);
+  error = signal('');
+  private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+
+  // constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient
-      .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(
-        map((resData) => resData.places)
-      )
-      .subscribe(
-        {
-          next: (places) => {
-            this.places.set(places);
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          },
-          error: (err) => {
-            console.log(err);
-            this.error.set("Try again later!");
-          }
-        }
-      );
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
   }
+
   onSelectPlace(selectedPlace: Place) {
-    this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: selectedPlace.id
-    }).subscribe({
-      next: (resData) => {
-        console.log(resData);
-      }
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace.id).subscribe({
+      next: (resData) => console.log(resData),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
     });
   }
 }
